f6d210986abbfc67f536af11bfdf06b93cde8be3
Zuckonit <lsin30foxmail.com>
2014-03-25 04:00:18 +0800

    init

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 todo.sh
 =======
+> simple todo-list script, which is also used for my conky
 
-simple todo-list script, which is also used for my conky
+Usage
+======
+> bash todo.sh help
+
+screenshots
+======
+![todo.png](https://raw.githubusercontent.com/Zuckonit/todo.sh/master/todo.png)
+
+
---
+++ todo.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+TODO_DIR=$HOME/.vim-airline-todo  #you can set your own todo directory
+[ ! -d $TODO_DIR ] && mkdir $TODO_DIR
+
+
+case $1 in
+    "list")
+        ls -tr "$TODO_DIR" | head -$2
+        ;;
+    "count_done")
+        ls $TODO_DIR | grep -E "^\[√" |  wc -l
+        ;;
+    "count_undone")
+        ls $TODO_DIR | grep -vE "^\[√" |  wc -l
+        ;;
+    "done")
+        ls $TODO_DIR | grep -E "^$2$" 
+        if [ $? -ne 0 ];then
+            echo "no such task"
+        else
+            mv $TODO_DIR/"$2" $TODO_DIR/"[√]$2"
+        fi
+        ;;
+    "new")
+        ls $TODO_DIR | grep -iE "^$2$" 
+        if [ $? -ne 0 ];then
+            echo "task alreay existed"
+        else
+            touch $TODO_DIR/"$2"
+        fi
+        ;;
+    "help")
+        echo "
+        count_done         show the count of done tasks
+        count_undone       show the count of undone tasks
+        done  [task_name]  mark the special task as done 
+        list  [count]      show the tasks of special count, count is optional
+        new   [task_name]  create a task with special name"
+        ;;
+esac
